d28753746406861a75910f322bbfb91df1bc6819
sherlyhu28 <sherlyhu28@gmail.com>
2023-04-06 20:12:29 +0700

    change file name from dist to docs to upload on github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs'),
+        filename: "[name].bundle.js",
     },
     module: {
         rules: [
